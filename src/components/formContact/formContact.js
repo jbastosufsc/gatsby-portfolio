e72d136a1c032a18de60fc25ca2a1dea94a8e3d3
jbastos.ufsc <jbastos.ufsc@gmail.com>
2020-02-14 11:03:56 -0300

    form contact finally working

--- src/components/formContact/formContact.js
+++ src/components/formContact/formContact.js
@@ -4,30 +4,39 @@ const FormContact = () => {
   return (
     <form
       name="contact"
-      action="/"
-      method="POST"
-      data-netlify-recaptcha="true"
+      method="post"
       data-netlify="true"
+      data-netlify-honeypot="bot-field"
     >
+      <input type="hidden" name="form-name" value="contact" />
+
+      <div hidden>
+        <label>
+          Don’t fill this out: <input name="bot-field" />
+        </label>
+      </div>
+
       <p>
         <label>
-          Your Name: <input type="text" name="name" />
+          Your Name: <input type="text" name="name" id="name" required="true" />
         </label>
       </p>
+
       <p>
         <label>
           Your Email: <input type="email" name="email" />
         </label>
       </p>
+
       <p>
         <label>
           Message: <textarea name="message"></textarea>
         </label>
       </p>
-      <div data-netlify-recaptcha="true"></div>
-      <p>
-        <button type="submit">Send</button>
-      </p>
+
+      <div>
+        <button type="submit">Enviar</button>
+      </div>
     </form>
   )
 }
